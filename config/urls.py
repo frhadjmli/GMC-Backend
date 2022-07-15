7d72e381103e1af16f7345ec97421442262ec175
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-15 13:36:53 +0430

    add new route for authentication

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', obtain_auth_token),
     path('', include('greenhouse.urls')),
 ]
