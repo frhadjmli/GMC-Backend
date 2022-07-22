9635808ec416c8a95f107e8b277c2a743260f294
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-22 14:41:44 +0430

    add some View in greenhouse\views.py & use it in greenhouse\urls.py
    -HumdSensorView
    -LuxSensorView
    -VentilationView
    -IrrigationView

--- greenhouse/urls.py
+++ greenhouse/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TempSensorView
+from .views import TempSensorView, HumdSensorView, LuxSensorView, VentilationView, IrrigationView
 
 urlpatterns = [
     path('tempSensor/', TempSensorView.as_view()),
+    path('HumdSensor/', HumdSensorView.as_view()),
+    path('LuxSensor/', LuxSensorView.as_view()),
+    path('Ventilation/', VentilationView.as_view()),
+    path('Irrigation/', IrrigationView.as_view()),
 ]
--- greenhouse/views.py
+++ greenhouse/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
-from .serializers import TempSensorSerializer
-from .models import TempSensor
+from .serializers import TempSensorSerializer, HumdSensorSerializer, LuxSensorSerializer, VentilationSerializer, IrrigationSerializer
+from .models import TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
 
 
 # Create your views here.
@@ -33,3 +33,30 @@ class TempSensorView(APIView):
     #
     #     return Response({'message': serializer.errors})
 
+
+class HumdSensorView(APIView):
+    def get(self, request, *arg, **kwargs):
+        qs = HumdSensor.objects.all()
+        serializer = HumdSensorSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class LuxSensorView(APIView):
+    def get(self, request, *arg, **kwargs):
+        qs = LuxSensor.objects.all()
+        serializer = LuxSensorSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class VentilationView(APIView):
+    def get(self, request, *arg, **kwargs):
+        qs = Ventilation.objects.all()
+        serializer = VentilationSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class IrrigationView(APIView):
+    def get(self, request, *arg, **kwargs):
+        qs = Irrigation.objects.all()
+        serializer = IrrigationSerializer(qs, many=True)
+        return Response(serializer.data)
