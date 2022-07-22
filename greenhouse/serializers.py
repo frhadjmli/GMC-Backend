74f2f5eca68382421818e00c58fce80476d39e6e
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-22 14:40:19 +0430

    add some Serializer in greenhouse\serializers.py
    -HumdSensorSerializer
    -LuxSensorSerializer
    -VentilationSerializer
    -IrrigationSerializer

--- greenhouse/serializers.py
+++ greenhouse/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import TempSensor
+from .models import TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
 
 
 class TempSensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = TempSensor
+        fields = '__all__'
+
+
+class HumdSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HumdSensor
+        fields = '__all__'
+
+
+class LuxSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LuxSensor
+        fields = '__all__'
+
+
+class VentilationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ventilation
+        fields = '__all__'
+
+
+class IrrigationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Irrigation
         fields = '__all__'
