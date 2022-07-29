fef14d166a9f3bbaba1f60a69ead87d6e7a81270
farhad <farhad080.jamali@gmail.com>
2022-07-29 21:25:08 +0430

    add a channel and a new url path so the server can make request to client

--- config/asgi.py
+++ config/asgi.py
@@ -6,11 +6,22 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path, re_path
+import django_eventstream
+
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': URLRouter([
+        re_path('events/', AuthMiddlewareStack(
+            URLRouter(django_eventstream.routing.urlpatterns)
+        ), { 'channels' : ['data_monitoring'] }),
+        re_path(r'', get_asgi_application()),
+    ]),
+})
