df2f08d14c8b5e95f71d8ef214a6f7a019d72ca6
farhad <farhad080.jamali@gmail.com>
2022-07-29 21:10:39 +0430

    register models to admin panel

--- greenhouse/admin.py
+++ greenhouse/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import (Point, TempSensor, HumdSensor, LuxSensor, 
+Irrigation, Ventilation)
 # Register your models here.
+
+admin.site.register(Point)
+admin.site.register(TempSensor)
+admin.site.register(HumdSensor)
+admin.site.register(LuxSensor)
+admin.site.register(Irrigation)
+admin.site.register(Ventilation)
