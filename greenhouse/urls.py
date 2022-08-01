932eef1a7ec0f37c0c55d4f2c8ad3e34d74d0bdf
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-01 11:50:51 +0430

    add two route for toggle status field in each model

--- greenhouse/urls.py
+++ greenhouse/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('HumdSensor/', HumdSensorView.as_view()),
     path('LuxSensor/', LuxSensorView.as_view()),
     path('Ventilation/', VentilationView.as_view()),
+    path('Ventilation/update/<int:ventilation_id>/', VentilationView.as_view()),
     path('Irrigation/', IrrigationView.as_view()),
+    path('Irrigation/update/<int:irrigation_id>/', IrrigationView.as_view()),
 ]
