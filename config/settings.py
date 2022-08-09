5eafe4855a281a6aa02ae5f3dfad88f3a41113f9
farhad <farhad080.jamali@gmail.com>
2022-08-09 13:39:21 +0430

    overwrite ALLOWED_HOSTS to accept connection from other devices on the network

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uk&c5zkb5=)o6o!oyzhc-zeorep3#(8ipof&4z4h99!m0pvcux
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.102']
 
 
 # Application definition
