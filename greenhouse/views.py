56a86c6c90bfc91eb3dbcee9aa60915c2f8459a2
farhad <farhad080.jamali@gmail.com>
2022-08-09 13:36:17 +0430

    add post method to sensor views to handle RP board requests

--- greenhouse/views.py
+++ greenhouse/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 
 from .serializers import (TempSensorSerializer, HumdSensorSerializer, LuxSensorSerializer,
  VentilationSerializer, IrrigationSerializer)
-from .models import TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
+from .models import Point, TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
 
 
 # Create your views here.
@@ -26,12 +27,37 @@ class TempSensorView(APIView):
         serializer = TempSensorSerializer(qs, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        temperature = TempSensor.objects.create(
+            sensor_id = request.data.get('sensor_id'),
+            temp_value = request.data.get('temp_value'),
+            recorded_time = request.data.get('current_time'),
+            date_time = request.data.get('current_date'),
+            point = Point.objects.get(id=request.data.get('point_id')),
+        )
+        return Response({
+            'Message':f"New Temperature({temperature.temp_value}) Registered"},
+            status=status.HTTP_201_CREATED
+        )
+
 class HumdSensorView(APIView):
     def get(self, request, *arg, **kwargs):
         qs = HumdSensor.objects.all()
         serializer = HumdSensorSerializer(qs, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        humidity = HumdSensor.objects.create(
+            sensor_id = request.data.get('sensor_id'),
+            humd_value = request.data.get('humd_value'),
+            recorded_time = request.data.get('current_time'),
+            date_time = request.data.get('current_date'),
+            point = Point.objects.get(id=request.data.get('point_id')),
+        )
+        return Response({
+            'Message':f"New Humidity({humidity.humd_value}) Registered"},
+            status=status.HTTP_201_CREATED
+        )
 
 class LuxSensorView(APIView):
     def get(self, request, *arg, **kwargs):
@@ -39,6 +65,18 @@ class LuxSensorView(APIView):
         serializer = LuxSensorSerializer(qs, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        lux = LuxSensor.objects.create(
+            sensor_id = request.data.get('sensor_id'),
+            lux_value = request.data.get('lux_value'),
+            recorded_time = request.data.get('current_time'),
+            date_time = request.data.get('current_date'),
+            point = Point.objects.get(id=request.data.get('point_id')),
+        )
+        return Response({
+            'Message':f"New Lux({lux.lux_value}) Registered"},
+            status=status.HTTP_201_CREATED
+            )
 
 class VentilationView(APIView):
     def get(self, request, *arg, **kwargs):
