6ee1761e29b68d6bad46e2b550c7e1951cf488ce
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-13 02:23:19 +0430

    add SensorValueSerializer, SensorSerializer in greenhouse\serializers.py

--- greenhouse/serializers.py
+++ greenhouse/serializers.py
@@ -1,32 +1,50 @@
 from rest_framework import serializers
-from .models import TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
+# from .models import TempSensor, HumdSensor, LuxSensor, Ventilation, Irrigation
+from .models import Sensor, Sensor_value
+
+# class TempSensorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = TempSensor
+#         fields = '__all__'
+#
+#
+# class HumdSensorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = HumdSensor
+#         fields = '__all__'
+#
+#
+# class LuxSensorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = LuxSensor
+#         fields = '__all__'
+#
+#
+# class VentilationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Ventilation
+#         fields = '__all__'
+#
+#
+# class IrrigationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Irrigation
+#         fields = '__all__'
 
 
-class TempSensorSerializer(serializers.ModelSerializer):
+class SensorValueSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TempSensor
+        model = Sensor_value
         fields = '__all__'
 
 
-class HumdSensorSerializer(serializers.ModelSerializer):
+class SensorSerializer(serializers.ModelSerializer):
+    sensor_values = SensorValueSerializer(many=True, read_only=True)
+
     class Meta:
-        model = HumdSensor
-        fields = '__all__'
+        model = Sensor
+        fields = ['id', 'name', 'sensor_type', 'point', 'sensor_values']
+        depth = 2
 
 
-class LuxSensorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LuxSensor
-        fields = '__all__'
 
-
-class VentilationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ventilation
-        fields = '__all__'
-
-
-class IrrigationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Irrigation
-        fields = '__all__'
