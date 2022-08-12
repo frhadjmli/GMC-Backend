aa93de140259c1d38a84e36f651fea02ba808d80
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-13 02:20:42 +0430

    register models in greenhouse\admin.py

--- greenhouse/admin.py
+++ greenhouse/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from .models import (Point, TempSensor, HumdSensor, LuxSensor, 
-Irrigation, Ventilation)
-# Register your models here.
+# from .models import (Point, TempSensor, HumdSensor, LuxSensor,
+# Irrigation, Ventilation)
+from .models import Point, Sensor_type, Sensor, Sensor_value, Device_type, Device, Device_value, Alaem_message
+
+# admin.site.register(Point)
+# admin.site.register(TempSensor)
+# admin.site.register(HumdSensor)
+# admin.site.register(LuxSensor)
+# admin.site.register(Irrigation)
+# admin.site.register(Ventilation)
 
 admin.site.register(Point)
-admin.site.register(TempSensor)
-admin.site.register(HumdSensor)
-admin.site.register(LuxSensor)
-admin.site.register(Irrigation)
-admin.site.register(Ventilation)
+admin.site.register(Sensor_type)
+admin.site.register(Sensor)
+admin.site.register(Sensor_value)
+admin.site.register(Device_type)
+admin.site.register(Device)
+admin.site.register(Device_value)
+admin.site.register(Alaem_message)
