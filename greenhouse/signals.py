1e83c4de1bbc68627f1e61f5552ad273fdd85c65
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-14 16:54:03 +0430

    Update signals.py
    instance refers to SensorValue

--- greenhouse/signals.py
+++ greenhouse/signals.py
@@ -8,7 +8,7 @@ from django_eventstream import send_event
 @receiver(post_save, sender=SensorValue)
 def send_temperature(sender, instance, created, **kwargs):
     if created:
-        sensor = get_object_or_404(Sensor, id=instance.id)
+        sensor = get_object_or_404(Sensor, id=instance.sensor_id)
         if sensor.sensor_id[:3] == 'TMP':
             send_event('data_monitoring', 'temp_update', {
                 'value': instance.value,
