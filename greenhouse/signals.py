c6d0aca603ffe2c52f6c273c89ca506ec53253ee
farhad <farhad080.jamali@gmail.com>
2022-08-14 12:30:28 +0430

    change signals based on new model fields

--- greenhouse/signals.py
+++ greenhouse/signals.py
@@ -1,32 +1,30 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from .models import SensorValue, Sensor, DeviceValue, Device
+from django.shortcuts import get_object_or_404
 
 from django_eventstream import send_event
 
-# @receiver(post_save, sender=TempSensor)
-# def send_temperature(sender, instance, created, **kwargs):
-#     if created:
-#         send_event('data_monitoring', 'temp_update', {
-#             'temp_value': instance.temp_value,
-#             'id': instance.id,
-#             'recorded_time': str(instance.recorded_time)
-#             })
-
-# @receiver(post_save, sender=HumdSensor)
-# def send_humidity(sender, instance, created, **kwargs):
-#     if created:
-#         send_event('data_monitoring', 'humd_update', {
-#             'humd_value': instance.humd_value,
-#             'id': instance.id,
-#             'recorded_time': str(instance.recorded_time)
-#             })
-
-# @receiver(post_save, sender=LuxSensor)
-# def send_lux(sender, instance, created, **kwargs):
-#     if created:
-#         send_event('data_monitoring', 'lux_update', {
-#             'lux_value': instance.lux_value,
-#             'id': instance.id,
-#             'recorded_time': str(instance.recorded_time)
-#             })
+@receiver(post_save, sender=SensorValue)
+def send_temperature(sender, instance, created, **kwargs):
+    if created:
+        sensor = get_object_or_404(Sensor, id=instance.id)
+        if sensor.sensor_id[:3] == 'TMP':
+            send_event('data_monitoring', 'temp_update', {
+                'value': instance.value,
+                'id': instance.id,
+                'recorded_time': str(instance.recorded_time)
+                })
+        elif sensor.sensor_id[:3] == 'HUM':
+            send_event('data_monitoring', 'humd_update', {
+                'value': instance.value,
+                'id': instance.id,
+                'recorded_time': str(instance.recorded_time)
+                })
+        elif sensor.sensor_id[:3] == 'LUX':
+            send_event('data_monitoring', 'lux_update', {
+                'value': instance.value,
+                'id': instance.id,
+                'recorded_time': str(instance.recorded_time)
+                })
   
