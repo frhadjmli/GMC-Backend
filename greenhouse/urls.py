2aa66a4b74328ca437cf813f4c85be917dff0c1b
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-14 16:55:54 +0430

    add SensorValueInfoView, DeviceValueInfoView
    changed to dynamic class view

--- greenhouse/urls.py
+++ greenhouse/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from .views import TempSensorView, HumidSensorView, LuxSensorView, VentilationView, IrrigationView
-from .views import SensorView, SensorValueView
+# from .views import TempSensorView, HumidSensorView, LuxSensorView, VentilationView, IrrigationView
+# from .views import SensorView, SensorValueView
+from .views import SensorValueInfoView, DeviceValueInfoView
 
 urlpatterns = [
-    path('sensor/', SensorView.as_view()),
-    path('sensorValue/', SensorValueView.as_view()),
+    # path('sensor/', SensorView.as_view()),
+    # path('sensorValue/', SensorValueView.as_view()),
 
-    path('tempSensor/', TempSensorView.as_view()),
-    path('HumdSensor/', HumidSensorView.as_view()),
-    path('LuxSensor/', LuxSensorView.as_view()),
-    path('Ventilation/', VentilationView.as_view()),
-    path('Ventilation/update/<int:device_id>/', VentilationView.as_view()),
-    path('Irrigation/', IrrigationView.as_view()),
-    path('Irrigation/update/<int:device_id>/', IrrigationView.as_view()),
+    path('SensorValueInfo/<int:sensor_type_id>/', SensorValueInfoView.as_view()),
+    path('SensorValueInfo/', SensorValueInfoView.as_view()),
+
+    # path('HumdSensor/', HumidSensorView.as_view()),
+    # path('LuxSensor/', LuxSensorView.as_view()),
+    path('DeviceValueInfo/<int:device_type_id>/', DeviceValueInfoView.as_view()),
+    path('DeviceValueInfo/update/<int:device_id>/', DeviceValueInfoView.as_view()),
+    # path('Irrigation/', IrrigationView.as_view()),
+    # path('Irrigation/update/<int:device_id>/', IrrigationView.as_view()),
  ]
