2c7396f37e1f575992657dbc4ef48882bddfeec6
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-16 21:30:10 +0430

    Create utils.py
    -retrive a range of values from db in retrive_renge_val()
    -if value greater or less than reng_val , create AlarmMessage in
     detect_alarm()

---
+++ greenhouse/utils.py
@@ -0,0 +1,30 @@
+from django.db.models import F
+from .models import Sensor, AlarmMessage, SensorTypeRange
+
+
+def retrive_renge_val():
+    range_value = {
+        # 'Temperature': [],
+        # 'Humidity': [],
+        # 'Lux': []
+    }
+    qs = SensorTypeRange.objects.select_related('sensortype').annotate(title=F('sensor_type_id__title')).values()
+    for item in list(qs):
+        range_value[item['title']] = []
+        range_value[item['title']].append(item['min_range'])
+        range_value[item['title']].append(item['max_range'])
+    return range_value
+
+
+def detect_alarm(range_val, instance, message=""):
+
+    if instance.value < range_val[0]:
+        message = message + "کم تر از حد مجاز"
+        AlarmMessage.objects.create(body_text=message, sensor=Sensor.objects.get(id=instance.sensor_id),
+                                    recorded_time=instance.recorded_time, date_time=instance.date_time)
+
+    elif instance.value > range_val[1]:
+        message = message + "بیشتر تر از حد مجاز"
+        AlarmMessage.objects.create(body_text=message, sensor=Sensor.objects.get(id=instance.sensor_id),
+                                    recorded_time=instance.recorded_time, date_time=instance.date_time)
+    return
