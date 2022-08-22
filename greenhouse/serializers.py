f009264f83fca9daaf906d6381b97dff4ba34d95
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-22 16:33:43 +0430

    add validator in SensorTypeRangeSerializer

--- greenhouse/serializers.py
+++ greenhouse/serializers.py
@@ -7,3 +7,22 @@ class SensorTypeRangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SensorTypeRange
         exclude = ['sensor_type']
+
+    def validate(self, data):
+        if data.get('min_range', 0) > data.get('max_range', 0):
+            error = 'Maximum range should be greater than minimum range'
+            raise serializers.ValidationError(error)
+
+        return data
+
+    def validate_min_range(self, value):
+        if value < 0:
+            raise serializers.ValidationError('Minimum range must be postive')
+
+        return value
+
+    def validate_max_range(self, value):
+        if value < 0:
+            raise serializers.ValidationError('Maximum range must be postive')
+
+        return value
